05ffa516c0b33e0c00cff042a21fdbee500d7ee2
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-11-02 23:02:10 -0400

    componenets initial styling for the pages

--- login_signup/src/App.js
+++ login_signup/src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
-import React from 'react';
-
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <div className="col-6 one">One</div>
-      <div className="col-6 two">Two</div>
-      <div className="col-6 three">Three</div>
-      <div className="col-6 four">Four</div>
-      <div className="col-6 five">Five</div>
-      <div className="col-6 six">Six</div>
-
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 one">One</div>
+          <div className="col-lg-6 two">Two</div>
+          <div className="col-lg-6 three">Three</div>
+          <div className="col-lg-6 four">Four</div>
+          <div className="col-lg-6 five">Five</div>
+          <div className="col-lg-6 six">Six</div>
+        </div>
+      </div>
     </div>
   );
 }
