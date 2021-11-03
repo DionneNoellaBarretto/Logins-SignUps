a7c9a11c16dc3ca01cf0cd222e1a989f5eb6f607
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-11-02 22:50:12 -0400

    initial commit

--- login_signup/src/App.js
+++ login_signup/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="col-6 one">One</div>
+      <div className="col-6 two">Two</div>
+      <div className="col-6 three">Three</div>
+      <div className="col-6 four">Four</div>
+      <div className="col-6 five">Five</div>
+      <div className="col-6 six">Six</div>
+
     </div>
   );
 }
