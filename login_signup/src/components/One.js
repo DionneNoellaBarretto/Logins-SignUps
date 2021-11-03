5a54488ba6d92498f022050b8d272310615c5603
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-11-02 23:22:26 -0400

    logging offf for the day

--- login_signup/src/components/One.js
+++ login_signup/src/components/One.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function One() {
-    return (
-        <div>
-            One
-        </div>
-    )
+  const password = useModel("");
+  const confirmPassword = useModel("");
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Input
+        model={password}
+        required
+        pattern={regex.moderatePassword}
+        placeholder="Password"
+      />
+      <Input
+        model={confirmPassword}
+        // passing the model that should be imprinted/followed
+        imprint-model={password}
+        required
+        placeholder="Confirm Password"
+      />
+
+      <button type="submit">Sign Up</button>
+    </Form>
+  );
 }
