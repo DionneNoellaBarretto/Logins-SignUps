ea1f42f265b2e0e745582e4e695465ead2a4e604
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-11-02 23:06:04 -0400

    componenets placed

--- login_signup/src/App.js
+++ login_signup/src/App.js
@@ -1,17 +1,23 @@
 import "./App.css";
 import React from "react";
+import {One} from "./components/One";
+import {Two} from "./components/Two";
+import {Three} from "./components/Three";
+import {Four} from "./components/Four";
+import {Five} from "./components/Five";
+import {Six} from "./components/Six";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <div className="row">
-          <div className="col-lg-6 one">One</div>
-          <div className="col-lg-6 two">Two</div>
-          <div className="col-lg-6 three">Three</div>
-          <div className="col-lg-6 four">Four</div>
-          <div className="col-lg-6 five">Five</div>
-          <div className="col-lg-6 six">Six</div>
+          <div className="col-lg-6 one"> <One /> </div>
+          <div className="col-lg-6 two"><Two /></div>
+          <div className="col-lg-6 three"><Three /></div>
+          <div className="col-lg-6 four"><Four /></div>
+          <div className="col-lg-6 five"><Five /></div>
+          <div className="col-lg-6 six"><Six /></div>
         </div>
       </div>
     </div>
---
+++ login_signup/src/components/Two.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Two() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
