53b9c232a26746c7c836e938dd955361fcdfc59b
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-11-03 23:25:51 -0400

    material ui login /signup 2nd layout page completed

--- login_signup/src/components/Two.js
+++ login_signup/src/components/Two.js
@@ -1,47 +1,95 @@
-import React, { Component } from "react";
-import "App.css";
-import CustomInput from "components/CustomInput";
-import Button from "components/Button";
+//https://levelup.gitconnected.com/create-a-signup-page-with-react-and-material-ui-9b203d18cf3f
 
-//https://codesandbox.io/s/login-form-material-ui-u1xjl?from-embed=&file=/src/assets/jss/material-kit-react.js
-export default class App extends Component {
-  state = {
-    email: "",
-    password: ""
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+    
+
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '300px',
+      border: '0.1px solid',
+    },
+    '& .MuiButtonBase-root': {
+      margin: theme.spacing(2),
+    },
+  },
+}));
+
+const Form = ({ handleClose }) => {
+  const classes = useStyles();
+  // create state variables for each input
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [reTypePassword, setPasswordAgain] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(firstName, lastName, email, password,reTypePassword);
+    handleClose();
   };
 
-  handleChange = e => {
-    this.setState({ [e.currentTarget.id]: e.currentTarget.value });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <form className="form">
-          <CustomInput
-            labelText="Email"
-            id="email"
-            formControlProps={{
-              fullWidth: true
-            }}
-            handleChange={this.handleChange}
-            type="text"
-          />
-          <CustomInput
-            labelText="Password"
-            id="password"
-            formControlProps={{
-              fullWidth: true
-            }}
-            handleChange={this.handleChange}
-            type="password"
-          />
-
-          <Button type="button" color="primary" className="form__custom-button">
-            Log in
-          </Button>
-        </form>
+  return (
+    <form className={classes.root} onSubmit={handleSubmit}>
+      <TextField
+        label="First Name"
+        variant="filled"
+        required
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+      />
+      <TextField
+        label="Last Name"
+        variant="filled"
+        required
+        value={lastName}
+        onChange={e => setLastName(e.target.value)}
+      />
+      <TextField
+        label="Email"
+        variant="filled"
+        type="email"
+        required
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <TextField
+        label="Password"
+        variant="filled"
+        type="password"
+        required
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <TextField
+        label="Retype Password"
+        variant="filled"
+        type="password"
+        required
+        value={password}
+        onChange={e => setPasswordAgain(e.target.value)}
+      />
+      <div>
+        <Button variant="contained" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button type="submit" variant="contained" color="primary">
+          Signup
+        </Button>
       </div>
-    );
-  }
-}
+    </form>
+  );
+};
+
+export default Form;
